0918a4b4c0d285f6578f7c78d335b05015cc8f3b
Franklin Germán Quihuang <quihuang2017@gmail.com>
2024-05-28 17:08:18 -0500

    Se eliminan archivos inecesarios y se actualiza ruta de la conexion para proceder con la creacion de la imgagen

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Electrodomestico = mongoose.model('Electrodomestico', new mongoose.Schema(
 
 const app = express()
 
-mongoose.connect('mongodb://admin:admin@localhost:27017/miapp?authSource=admin')
+mongoose.connect('mongodb://admin:admin@container-mongo:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
   console.log('listando... Electrodomesticos...')
@@ -38,3 +38,4 @@ app.get('/eliminar/:id', async (req, res) => {
 });
 
 app.listen(3000, () => console.log('listening...'))
+
